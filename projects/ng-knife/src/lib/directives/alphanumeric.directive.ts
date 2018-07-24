1a3793b9f0fd881175154103a145eae8bb44885c
Marcelo Canales <develop.marcelocanales@gmail.com>
2018-07-23 22:21:49 -0400

    Optimization and fixs

--- projects/ng-knife/src/lib/directives/alphanumeric.directive.ts
+++ projects/ng-knife/src/lib/directives/alphanumeric.directive.ts
@@ -6,10 +6,8 @@ import { Directive, HostListener } from '@angular/core';
 export class AlphanumericDirective {
 
   @HostListener('keydown', ['$event']) onKeyDown(e: KeyboardEvent): boolean {
-    console.log(e.code);
-    console.log(e.key);
-    console.log(e.keyCode);
-    return (e.keyCode === 32 || e.keyCode === 37 || e.keyCode === 39) ? true : (new RegExp('^[a-zA-Z0-9_]*$')).test(e.key);
+    return (e.keyCode === 8 || e.keyCode === 32 || e.keyCode === 37 || e.keyCode === 39)
+    ? true : (new RegExp('^[a-zA-Z0-9_]*$')).test(e.key);
   }
 
 }
