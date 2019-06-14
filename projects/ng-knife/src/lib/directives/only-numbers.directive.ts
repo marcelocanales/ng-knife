dee2948e9e3aa5c26d2ac986c3b4a2d9083800a2
Marcelo Canales <marcelo.canales@alaya.cl>
2019-06-14 15:24:21 -0400

    Fix paste knifeOnlyNumbers directive

--- projects/ng-knife/src/lib/directives/only-numbers.directive.ts
+++ projects/ng-knife/src/lib/directives/only-numbers.directive.ts
@@ -5,7 +5,15 @@ import { Directive, HostListener } from '@angular/core';
 })
 export class OnlyNumbersDirective {
 
+  onlyNumbersRegExp: RegExp = new RegExp('^\\d+$');
+
+  @HostListener('paste', ['$event']) pasteClipboard(e: ClipboardEvent): boolean {
+    const data = e.clipboardData.getData('text');
+    return this.onlyNumbersRegExp.test(data) ? true : false;
+  }
+
   @HostListener('keydown', ['$event']) onKeyDown(e: KeyboardEvent): boolean {
-    return (e.keyCode === 8 || e.keyCode === 37 || e.keyCode === 39 || e.keyCode === 46) ? true : (new RegExp('^\\d+$')).test(e.key);
+    return (e.ctrlKey || e.metaKey || e.keyCode === 8 || e.keyCode === 9 || e.keyCode === 37 || e.keyCode === 39 ||
+       e.keyCode === 91 || e.keyCode === 93) ? true : this.onlyNumbersRegExp.test(e.key);
   }
 }
